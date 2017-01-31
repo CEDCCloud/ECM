9c366f5e64def1eb3abcc7079d2560e370f8541a
pemazzon <paoloemilio.mazzon@gmail.com>
2017-01-31 15:14:35 +0100

    Commit iniziale dopo il fork. Correzioni banali.

--- ecm.py
+++ ecm.py
@@ -5,7 +5,7 @@ from subprocess import call
 scripts = [(1, "Scientific Linux 6", "scientific", "master_files/SL6-master"),
            (2, "Ubuntu", "ubuntu", "master_files/Ubuntu-master"),
            (3, "uCernVM", "ucernvm", "master_files/uCernVM-master"),
-           (4, "CentOS 7", "centos", "master_files/CentOS7-master")]
+           (4, "CentOS", "centos", "master_files/CentOS7-master")]
            
 SL_images = [(1, "SL68-x86_64-20161107", "ami-6b1406fc"),
           (2, "SL67-x86_64-20151017", "ami-09877a78"),
@@ -15,17 +15,15 @@ SL_images = [(1, "SL68-x86_64-20161107", "ami-6b1406fc"),
           (6, "SL65-x86_64-20151029", "ami-d760f202"),
           (7, "Other image. [WARNING] You have to know the EC2-id of image", "ami-")]
           
-ubuntu_images = [(1, "ubuntu-trusty-20141025", "ami-84616f8f"),
-          (2, "Other image. [WARNING] You have to know the EC2-id of image", "ami-")]
+ubuntu_images = [(1, "ubuntu-16.04.1-LTS", "ami-000000c6"),
+          (2, "ubuntu-14.04.3-LTS", "ami-0000001b"),
+          (3, "Other image. [WARNING] You have to know the EC2-id of image", "ami-")]
           
-cern_images = [(1, "uCernVM 3.4.3", "ami-fcfdc38d"),
-          (2, "uCernVM 2.3-0", "ami-c84d00a7"),
-          (3, "uCernVM 1.18.14", "ami-aebb6e5f"),
-          (4, "uCernVM 1.18.13", "ami-7ad4ec84"),
-          (5, "Other image. [WARNING] You have to know the EC2-id of image", "ami-")]
+cern_images = [(1, "uCernVM 3", "ami-0000005a"),
+          (2, "Other image. [WARNING] You have to know the EC2-id of image", "ami-")]
 
-centos_images = [(1, "CentOS 7", "ami-9f3da3fc"),
-                 (2, "CentOS-7-x86_64-GenericCloud-1608", "ami-3f901eed"),
+centos_images = [(1, "CentOS 6", "ami-00000010"),
+                 (2, "CentOS 7", "ami-00000013"),
                  (3, "Other image. [WARNING] You have to know the EC2-id of image", "ami-")]
            
 if __name__ == "__main__":
@@ -55,10 +53,10 @@ if __name__ == "__main__":
     idle_time = params['IDLE_TIME']
     key_name = params['KEY_NAME']
 
-    print("\nChoose the Operating System (OS) type that you want to use for your master and worker nodes:")
+    print("\nChoose the Operating System (OS) you want \nto use for your master and worker nodes:\n")
     for n, d, so, f in scripts:
         print("%s: %s" % (n, d))
-    which = input("OS type ?\n")
+    which = input("\nOS type => ")
     ctrl=int(which)
 
     try:
@@ -73,7 +71,7 @@ if __name__ == "__main__":
                 print("\nSelect the image for your %s based master and your %s based WNs:" %(d, d)) 
                 for m, b, g in SL_images:
                     print("%s: %s" % (m, b))
-                image = input()
+                image = input("\nImage => ")
                 try:
                     m, b, g = SL_images[int(image)-1]
                     if int(image) == 7:
@@ -96,7 +94,7 @@ if __name__ == "__main__":
                 print("\nSelect the image for your %s based master and your %s based WNs:" %(d, d)) 
                 for m, b, g in ubuntu_images:
                     print("%s: %s" % (m, b))
-                image = input()
+                image = input("\nImage => ")
                 try:
                     m, b, g = ubuntu_images[int(image)-1]
                     if int(image) == 2:
@@ -119,7 +117,7 @@ if __name__ == "__main__":
                 print("\nSelect the image for your %s based master and your %s based WNs:" %(d, d)) 
                 for m, b, g in cern_images:
                     print("%s: %s" % (m, b))
-                image = input()
+                image = input("\nImage => ")
                 try:
                     m, b, g = cern_images[int(image)-1]
                     if int(image) == 5:
@@ -142,7 +140,7 @@ if __name__ == "__main__":
                 print("\nSelect the image for your %s based master and your %s based WNs:" %(d, d)) 
                 for m, b, g in centos_images:
                     print("%s: %s" % (m, b))
-                image = input()
+                image = input("\nImage => ")
                 try:
                     m, b, g = centos_images[int(image)-1]
                     if int(image) == 3:
